439328678bf65eec2c9a2abce05c2c8034a630fd
William_Hernandez <william2hernandezh.dez@gmail.com>
2025-05-26 21:20:52 -0600

    miniDesafioLunes

---
+++ Semana 1/Lunes/EjercicioLunes.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("esta es la prueba del lunes");
+Console.WriteLine("son solo lineas de texto en consola");
+var int numero = 10;// cuando se usa var no es necesario especificar el tipo de dato
+// solo se usa el tipo de dato cuando la variable se declara explicitamente como en la linea siguiente
+string texto = "esto es un texto";
+Console.WriteLine($"tambien se pueden concatenar variables como" + numero + " y " + texto);
+Console.WriteLine("con la ayuda del copilot concatene de esta manera pero no recuerdo para que sirve el $");
+Console.WriteLine($"esto por ejemplo no recuerdo que era, me ayudas porfa explicando todos estos temas para entender o no se si sera parte de otro tema mas adelante {texto}");
---
+++ Semana 1/Lunes/ejercioExtra.cs
@@ -0,0 +1,7 @@
+string name = "Yesica";
+string lastName = "Gonzalez";
+int age = 30;
+string city = "Buenos Aires";
+string profecion = "Ingeniera de Software";
+Console.WriteLine($"Hola, mi nombre es {name} {lastName}, tengo {age} años, vivo en {city} y soy {profecion}.");
+Console.WriteLine("Esta es una prueba del lunes");
